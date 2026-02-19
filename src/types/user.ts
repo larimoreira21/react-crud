e96c391431cc740bdcbb5f27864651afe1ddee75
Larissa Moreira <larissamoreira@MacBook-Air.local>
2026-02-19 15:17:32 -0300

    chore: user type

---
+++ src/types/user.ts
@@ -0,0 +1,17 @@
+export type User = {
+  id?: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  jobArea: string;
+  jobTitle: string;
+  jobType: string;
+  jobDescription: string;
+  avatar: string;
+  country: string;
+  city: string;
+  streetAdress: string;
+  zipCode: string;
+  state: string;
+};
